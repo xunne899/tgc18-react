f588b076471405c09fbe7186b59bdf10d0694853
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-06-24 06:49:43 +0000

    new handson

---
+++ 01-basics/src/App.js
@@ -0,0 +1,24 @@
+// import for one page only 
+import React from 'react'; // const React = require('react)
+import logo from './logo.svg'
+import'./style.css'
+
+//creates a React component
+// App.js is default entry point to all React application
+function App(){
+  //a React component always must return JSX
+  // only one parent is allowed ---  so can lump 2 or more elements under a div or fragment
+  return(
+    <React.Fragment>
+      <h1 style={{
+        color:"green",
+        backgroundColor:"yellow"
+      }}>Hello World</h1>
+    <p>Let there be light</p>
+    <img src={logo}/>
+    <img src={require('./dog.jpg')}/>
+    </React.Fragment>
+  )
+}
+// export out component 
+export default App // module.exports =App
---
+++ handson-1.1-tryout/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'; // const React = require('react)
+import'./style.css'
+
+
+export default function App(){
+
+  return(
+    <React.Fragment>
+   <body>
+    <nav>
+      <ul id="menu">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Our Menu</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+    </nav>
+    <section id="hero">
+      <div id="cta">
+        <a href="#">
+          Make a Booking
+        </a>
+      </div>
+    </section>
+    <section id="about-us">
+      <div class="content">
+        <h1>Cute dogs for adoption</h1>
+      </div>
+    </section>
+  </body>
+    </React.Fragment>
+  )
+}
+
