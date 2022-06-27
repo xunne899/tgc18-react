3b677b485d3ca2a94e45eb818462dd49fed7e009
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-06-27 03:28:42 +0000

    03-state

--- 03-state/src/NumberBox.js
+++ 03-state/src/NumberBox.js
@@ -1,8 +1,15 @@
-class NumberBox extends React.Component{
-//  class must have render 
+import React from 'react'
+
+export default class NumberBox extends React.Component{
+//  class must have render
+state ={
+    "count": 10
+}
+// 
 render(){
     return(
-        <div style={{ border:"1px solid black", padding:"10px", "width": "20px"}}>0</div>
+        // this.state.count refers to the Numberbox state
+        <div style={{ border:"1px solid black", padding:"10px", "width": "20px"}}>{this.state.count}</div>
     )
 }
 
