9735a0eeb7af101c28c34ab28fd7a471ea51be35
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-06-29 07:31:33 +0000

    add new 07 axios form

---
+++ 07-handson-forms-axios/src/SurveyForm.js
@@ -0,0 +1,349 @@
+import React from 'react'
+
+export default class SurveyForm extends React.Component{
+    state = {
+        'name':'',
+        'email':'',
+        'color':'',
+        'country':'',
+        'fruits':[],
+        'hasSubmitted':false
+    }
+
+
+getNameError = () =>{
+  if( this.state.name.length <3){
+    return "Name must have more than 3 characters"
+  } else if( this.state.name >20){
+    return "Name must not exceed 20 characters"
+  } else {
+    return null
+  }
+}
+
+
+getEmailError = () => {
+  if (this.state.email.includes('@') === false) {
+      return "The email is in the wrong format";
+  } else {
+      return null;
+  }
+}
+
+submit = () => {
+     
+  this.setState({
+      'hasSubmitted': true
+  })
+
+  // check if there is no error
+  if (this.getNameError()===null && this.getEmailError()===null) {
+      alert("All data is ok!")
+  } 
+}
+
+
+submit = () => {
+     
+  this.setState({
+      'hasSubmitted': true
+  })
+
+  // check if there is no error
+  if (this.getNameError()===null && this.getEmailError()===null) {
+      alert("All data is ok!")
+  } 
+}
+
+    updateFormField =(e)=>{
+        this.setState({
+         [e.target.name] :e.target.value
+        })
+       }
+
+       updateFruitsV = (e) =>{
+        // 1. check if the value is already in the array
+let currentValues = this.state[e.target.name];
+let modifiedValues;
+
+// 2. Check if the currently checked checkboxes' values include the target checkbox we just changed
+if (!currentValues.includes(e.target.value)) {
+// if the target checkbox's value does not exist in the array of currently checked values, it means we are checking the checkbox, so we add the value to the array
+	modifiedValues = [...currentValues, e.target.value];
+} else {
+	// the value is already in the array, means we are unchecking
+	// so we should remove the checkbox's value from the array
+	modifiedValues = currentValues.filter((element)=>{
+		// return true if we want to keep this particular element
+		// keep the element that we didn't just uncheck
+		return element !== e.target.value;
+	})
+}
+
+this.setState({
+	[e.target.name]: modifiedValues
+})
+}
+    
+      // the most straight forward way
+      // two cases:
+      // case 1: the value is already in the array
+      //          -> remove it from the array
+      // case 2: the value is not in the array
+      //          -> then we add it to the array
+      //values should be immutable --- cannot be changed
+
+      // updateFruitsSF = (evt) => {
+      //    // 1. clone
+      //    let clone = this.state.fruits.slice();
+    
+      //    // 2. modify the clone
+      //    // update cloned array
+      //   clone.push(evt.target.value)
+
+      //    // 3 replace
+      //    this.setState({
+      //      fruits: clone
+      //    });
+      //   }
+
+   // updateFruitsOneLine = (event) => {
+    //     // 1. clone the original array
+    //     // 2. update the cloned array
+    //     // let cloned = [...this.state.fruits, event.target.value];
+    //     // this.setState({
+    //     //     'fruits': cloned
+    //     // })
+
+    //     this.setState({
+    //         'fruits':[...this.state.fruits, event.target.value]
+    //     })
+    // }
+
+// for tick and untick
+    // updateFruitsOne = (event) => {
+
+
+    //   // check if the value is already in the array
+    //   // (i.e check if the checkbox has been checked)
+    //   if (this.state.fruits.includes(event.target.value)) {
+    //       // how to remove from an array
+          
+    //       // 1. clone the original array
+    //       let cloned = this.state.fruits.slice();
+
+    //       // 2. remove from the clone
+    //       let indexToRemove = -1;
+    //       for (let i =0; i < this.state.fruits.length; i++) {
+    //           if (this.state.fruits[i] === event.target.value) {
+    //               indexToRemove = i;
+    //               break;
+    //           }
+    //       }
+    //       cloned.splice(indexToRemove, 1);
+
+    //       // 3. replace the cloned array into the state
+    //       this.setState({
+    //           'fruits': cloned
+    //       })
+    //   } else {
+    //       // React community believes in values should be immutable
+    //       // this only applies to array
+  
+    //       // 1. clone the original array
+    //       let cloned = this.state.fruits.slice();
+
+    //       // 2. update the cloned array
+    //       cloned.push(event.target.value)
+
+    //       // 3. set the cloned array back into the state
+    //       this.setState({
+    //           'fruits': cloned
+    //       })
+    //   }   
+    // }
+
+
+
+  //   updateFruitsTwo = (event) => {
+
+  //     if (this.state.fruits.includes(event.target.value)) {
+  //         // removing
+  //         let indexToRemove = this.state.fruits.indexOf(event.target.value);
+  //         let cloned = [
+  //             ...this.state.fruits.slice(0, indexToRemove),
+  //             ...this.state.fruits.slice(indexToRemove+1)
+  //         ]
+  //         this.setState({
+  //             'fruits':cloned
+  //         })
+  //     } else {
+  //     // 1. clone the original array
+  //     // 2. update the cloned array
+  //     // let cloned = [...this.state.fruits, event.target.value];
+  //     // this.setState({
+  //     //     'fruits': cloned
+  //     // })
+
+  //     this.setState({
+  //         'fruits':[...this.state.fruits, event.target.value]
+  //     })
+
+  //     }
+      
+  // }
+
+
+
+      updateFruits = (evt) => {
+        if (this.state.fruits.includes(evt.target.value)) {
+          // case 1: the array already  have the value
+    
+          // 1. clone
+          let clone = this.state.fruits.slice();
+    
+          // 2. modify the clone
+          let indexToRemove = this.state.fruits.findIndex(function (fruit) {
+            return fruit === evt.target.value; // <-- evt.target.value is the value of the checkbox that has been just checked
+          });
+          clone.splice(indexToRemove, 1);
+        
+          // 3 replace
+          this.setState({
+            fruits: clone
+          });
+        } else {
+          // case 2: the array don't have the value
+          // 1. make a clone of the original array
+          let clone = this.state.fruits.slice();
+    
+          // 2. change the clone
+          clone.push(evt.target.value);
+    
+          // 3. replace the array in the state with the clone
+          this.setState({
+            fruits: clone
+          });
+        }
+      };
+    
+      updateFruitsV2 = (evt) => {
+        if (this.state.fruits.includes(evt.target.value)) {
+          let clone = this.state.fruits.filter(function (f) {
+            return f !== evt.target.value;
+          });
+          this.setState({
+            fruits: clone
+          });
+        } else {
+          // 1. clone
+          // eg. if fruits = ['apples','oranges']
+          const clone = [...this.state.fruits];
+          // => const clone = ['apples', 'oranges']
+    
+          // 2. modify
+          clone.push(evt.target.value);
+    
+          // 3. replace
+          this.setState({
+            fruits: clone
+          });
+        }
+      };
+    
+      updateFruitsV3 = (evt) => {
+        if (this.state.fruits.includes(evt.target.value)) {
+          // let indexToRemove = this.state.fruits.findIndex(
+          //   (f) => f === evt.target.value
+          // );
+          // let clone = [
+          //   ...this.state.fruits.slice(0, indexToRemove), // <-- only includes those before index to remove
+          //   ...this.state.fruits.slice(indexToRemove + 1) // <-- only includes those after index to remove
+          // ];
+          // this.setState({
+          //   fruits: clone
+          // });
+          let indexToRemove = this.state.fruits.findIndex(
+            (f) => f === evt.target.value
+          );
+          this.setState({
+            fruits: [
+              ...this.state.fruits.slice(0, indexToRemove),
+              ...this.state.fruits.slice(indexToRemove + 1)
+            ]
+          });
+        } else {
+          // const clone = [...this.state.fruits, evt.target.value];
+          // eg. if fruits = ['apples','oranges']
+          // then: const clone = [...this.state.fruits]
+          //    => const clone = ["apples", "oranges"]
+    
+          this.setState({
+            fruits: [...this.state.fruits, evt.target.value]
+          });
+        }
+      };
+
+
+
+
+render(){
+
+// // 1. check if the value is already in the array
+// let currentValues = this.state[e.target.name];
+// let modifiedValues;
+
+// // 2. Check if the currently checked checkboxes' values include the target checkbox we just changed
+// if (!currentValues.includes(e.target.value)) {
+// // if the target checkbox's value does not exist in the array of currently checked values, it means we are checking the checkbox, so we add the value to the array
+// 	modifiedValues = [...currentValues, e.target.value];
+// } else {
+// 	// the value is already in the array, means we are unchecking
+// 	// so we should remove the checkbox's value from the array
+// 	modifiedValues = currentValues.filter((element)=>{
+// 		// return true if we want to keep this particular element
+// 		// keep the element that we didn't just uncheck
+// 		return element !== e.target.value;
+// 	})
+// }
+
+// this.setState({
+// 	[e.target.name]: modifiedValues
+// })
+
+        return(
+            <React.Fragment>
+            <div>
+                <label>Name:</label>
+                <input name="name" type="text" value={this.state.name} onChange={this.updateFormField}/>{this.getNameError() && this.state.hasSubmitted ? <span className="error">{this.getNameError()}</span> : ""}
+            </div>
+            <div>
+                <label>Email:</label>
+                <input name="email" type="text" value={this.state.email} onChange={this.updateFormField}/>{this.getEmailError() && this.state.hasSubmitted ? <span className="error">{this.getEmailError()}</span> : ''}
+            </div>
+            <div>
+                <label>Favourite Color:</label>
+                <input name="color" type="radio" value="red" checked={this.state.color ==="red"} onChange={this.updateFormField}/>Red
+                <input name="color" type="radio" value="blue" checked={this.state.color ==="blue"} onChange={this.updateFormField}/>Blue
+                <input name="color" type="radio" value="green" checked={this.state.color ==="green"} onChange={this.updateFormField}/>Green
+            </div>
+            <div>
+                <label>Country:</label>
+                <select name="country" value={this.state.country} onChange={this.updateFormField}>
+                    <option value="singapore">Singapore</option>
+                    <option value="malaysia">Malaysia</option>
+                    <option value="indonesia">Indonesia</option>
+                </select>
+            </div>
+            <div>
+                <label>Fruits:</label>
+                <input type="checkbox" name="fruits" value="apple"  onChange={this.updateFruitsV} checked={this.state.fruits.includes('apple')}/>Apple
+                <input type="checkbox" name="fruits" value="orange" onChange={this.updateFruitsV} checked={this.state.fruits.includes('orange')}/>Orange
+                <input type="checkbox" name="fruits" value="banana" onChange={this.updateFruitsV} checked={this.state.fruits.includes('banana')}/>Banana
+                <input type="checkbox" name="fruits" value="durian" onChange={this.updateFruitsV} checked={this.state.fruits.includes('durian')}/>Durian
+            </div>
+            <button onClick={this.submit}>Submit</button>
+            </React.Fragment>
+        )
+    }
+}
---
+++ 07-handson-forms-axios/src/SurveyForm1.js
@@ -0,0 +1,183 @@
+import React from 'react'
+import axios from 'axios'
+export default class SurveyForm1 extends React.Component{
+    
+  
+  async componentDidMount(){
+    let enquiriesResponse = await axios.get('./enquiries.json');
+    let countryResponse = await axios.get('./countries.json');
+    let contactsResponse = await axios.get('./contacts.json');
+    this.setState({
+      allEnquiries: enquiriesResponse.data,
+      allCountries: countryResponse.data,
+      allContacts: contactsResponse.data,
+      loaded:true
+    })
+
+    //loading in parallel
+//     let enquiryRequest = axios.get('/enquiries.json');
+//     let countryRequest = axios.get('/countries.json');
+//     let contactRequest = axios.get('/contacts.json');
+// let [contactResponse, enquiryResponse, countryResponse]
+// = await axios.all([contactRequest, enquiryRequest, countryRequest]);
+// this.setState({
+// 'allCountries': countryResponse.data,
+// 'allEnquiries': enquiryResponse.data,
+// 'allContacts': contactResponse.data
+// })
+  }
+  
+  
+  state = {
+        firstname:'',
+        lastname:'',
+        enquiry:'',
+        country:'',
+        contacts:[],
+        allEnquiries:[],
+        allCountries:[],
+        allContacts:[],
+        loaded : false
+    
+  }
+
+
+ clickSubmit = () =>{
+  alert(`Firstname:${this.state.firstname}
+  Lastname:${this.state.lastname}
+  Enquiry:${this.state.enquiry}
+  Country:${this.state.country}
+  Contacts:${this.state.contacts}`)
+ }
+    updateFormField =(e) =>{
+      // let VarName = e.target.name
+        this.setState({
+            [e.target.name] : e.target.value
+            // [VarName] : e.target.value
+        })
+    }
+
+
+
+  //   updateEmail = (event) => {
+  //     this.setState({
+  //         'email': event.target.value
+  //     })
+  // }
+
+  // updateColour = (event) => {
+  //     this.setState({
+  //         'colour': event.target.value
+  //     })
+  // }
+
+  // updateCountry = (event) => {
+  //     this.setState({
+  //         'country': event.target.value
+  //     })
+  // }
+
+
+    renderEnquiry(){
+      let options =[];
+      for (let e of this.state.allEnquiries){
+        options.push(<React.Fragment key={e.value}>
+        <input type="radio"
+        name="enquiry"
+        value={e.value}
+        className="form-check-input"
+        onChange={this.updateFormField}
+        checked={this.state.enquiry === e.value}
+    />
+    <label className="form-check-label">{e.display}</label>
+        </React.Fragment>
+        )
+      }
+      return options;
+    }
+
+
+
+
+    updateContact = (e) => {
+        if (this.state.contacts.includes(e.target.value)) {
+          // case 1: the checkbox has already been checked, and now it's being unchecked
+          // 1. clone
+          const clone = this.state.contacts.slice();
+          // 2. modify (removing element at indexToremove)
+          const indexToRemove = this.state.contacts.findIndex(function (c) {
+            return c === e.target.value;
+          });
+          clone.splice(indexToRemove, 1);
+          // 3. replace
+          this.setState({
+            contacts: clone
+          });
+        } else {
+          // case 2: the checkbox has not been checked and now it's being checked
+          // 1. clone the array
+          // const clone = this.state.contacts.slice();
+          const clone = [...this.state.contacts];
+          // 2. modify the clone
+          clone.push(e.target.value);
+          // 3. replace the clone into state
+          this.setState({
+            contacts: clone
+          });
+        }
+      };
+
+render(){
+  if (this.state.loaded) {
+    return(
+        <React.Fragment>
+          <h1>Contact Form</h1>
+        <div>
+            <label>First Name:</label>
+            <input name="firstname" type="text" value={this.state.firstname} onChange={this.updateFormField}/>
+        </div>
+        <div>
+            <label>Last Name:</label>
+            <input name="lastname" type="text" value={this.state.lastname} onChange={this.updateFormField}/>
+        </div>
+        <div>
+            <label>Type of enquiry:</label>
+          {this.renderEnquiry()}
+        </div>
+        <div>
+            <label>Country:</label>
+            <select className="form-control" value={this.state.country} name="country" onChange={this.updateFormField}>
+                            {this.state.allCountries.map( c =><option value={c.value}  key={c.value}>
+                                {c.display}
+                            </option>)}
+    
+                            {/* {
+                        (() => {
+                            let options = [];
+                            for (let c of this.state.allCountries) {
+                                options.push(<option key={c.value} value={c.value}>{c.display}</option>)
+                            }
+                            return options;
+                        })()
+                    } */}
+
+                        </select>
+        </div>
+        <div>
+        <label>Contact Form:</label> 
+        {
+                            this.state.allContacts.map( c =><React.Fragment key={c.value}>
+                          
+                                <input type="checkbox" onChange={this.updateContact} className="form-check-input" value={c.value} checked={this.state.contacts.includes(c.value)} />
+                                <label className="form-check-label">{c.display}</label>
+                            </React.Fragment>)
+                        }
+        </div>
+        <button onClick = {this.clickSubmit}>Submit</button>
+        </React.Fragment>
+    )
+} else{
+  return <p>Loading... please wait</p>
+}
+}
+}
